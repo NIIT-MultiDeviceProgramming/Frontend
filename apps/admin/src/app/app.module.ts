319309b473aa273038dd0109dd1acb1af5c0773a
Rosy <rosychadha2018@gmail.com>
2022-04-08 23:20:01 +0530

    Categories form added'

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { HttpClientModule } from '@angular/common/http';
+import { CategoriesService } from '@shoppe/products';
+import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+
+const UX_MODULE = [
+    CardModule,
+    InputTextModule,
+    ToolbarModule,
+    ButtonModule,
+    TableModule,
+    ToastModule,
+]
+
 
 const routes: Routes = [
     {
@@ -15,20 +38,31 @@ const routes: Routes = [
         children : [
             {
                 path: 'dashboard',
-                component: DashboardComponent
+                component: DashboardComponent,
             },
             {
-                path: 'products',
-                component: DashboardComponent
+                path: 'categories',
+                component: CategoriesListComponent,
+            },
+            {
+                path: 'categories/form',
+                component: CategoriesListComponent,
             }
         ]
     }
 ];
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, DashboardComponent, ShellComponent, SidebarComponent],
-    imports: [BrowserModule, RouterModule.forRoot(routes, { initialNavigation: 'enabled' })],
-    providers: [],
+    declarations: [AppComponent, NxWelcomeComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent, CategoriesFormComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(routes, { initialNavigation: 'enabled' }), 
+        ...UX_MODULE],
+    providers: [CategoriesService, MessageService],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService, Category } from '@shoppe/products';
+
+@Component({
+  selector: 'shoppe-categories-list',
+  templateUrl: './categories-list.component.html',
+  styles: [
+  ]
+})
+export class CategoriesListComponent implements OnInit {
+
+  categories: Category[] = [];
+
+  constructor(private categoriesService: CategoriesService) { }
+
+  ngOnInit(): void {
+    this.categoriesService.getCategories().subscribe(cats => {
+      this.categories = cats;
+    })
+  }
+
+}
